8b22a4864d3731dc1ae879c9c0f1244822be345e
Tubagusva <124652119+Tubagusva@users.noreply.github.com>
2023-02-10 14:28:49 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+basic.showIcon(IconNames.Butterfly)
+basic.forever(function () {
+    if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Run, 60)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.Black) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.White)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinLeft, 60)
+    } else if (Tinybit.Line_Sensor(Tinybit.enPos.LeftState, Tinybit.enLineState.White) && Tinybit.Line_Sensor(Tinybit.enPos.RightState, Tinybit.enLineState.Black)) {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_SpinRight, 60)
+    } else {
+        Tinybit.CarCtrlSpeed(Tinybit.CarState.Car_Stop, 0)
+    }
+})
